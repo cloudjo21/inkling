6351decc80ab32c49b51f8d4351247eb870d045a
ed <cloudjo21@gmail.com>
2025-01-31 14:13:49 +0900

    chore: support extras_require

--- setup.py
+++ setup.py
@@ -3,6 +3,18 @@ from setuptools import setup, find_packages
 with open("requirements.txt") as f:
     required = f.read().splitlines()
 
+extras_require = {
+    "aws": [
+        "boto3",
+        "s3fs",
+        "apache-airflow-providers-amazon==9.1.0",
+    ],
+    "gcp": [
+        "apache-airflow-providers-google",
+        "pandas-gbq",
+    ]
+}
+
 setup(
     name="inkling",
     version="0.2.0",
@@ -12,4 +24,5 @@ setup(
     python_requires=">=3.11.6",
     long_description=open("README.md").read(),
     install_requires=required,
+    extras_require=extras_require,
 )
